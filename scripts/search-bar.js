ed3471231c4be085f781be5617043b1e24db9a96
beejq <bienjulius0@gmail.com>
2025-05-23 09:23:12 +0800

    Search Bar

--- scripts/search-bar.js
+++ scripts/search-bar.js
@@ -2,3 +2,8 @@ function showBox(id){
     const section = document.getElementById(id);
     section.classList.toggle('active');
 }
+
+function collapseFilter(id) {
+    const section = document.getElementById(id);
+    section.classList.toggle('active');
+}
